6db44f23998e4a9d7cca638a0c864909b72023dc
moinultm <moinultm@gmail.com>
2019-08-17 20:37:15 +0600

    adds payment

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
+use App\Traits\Paginator;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    use Paginator;
+
+    public function index(Request $request): JsonResponse
+    {
+        $query = Product::query();
+        return response()->json(self::paginate($query, $request), 200);
+    }
 }
