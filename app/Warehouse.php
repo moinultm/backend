83d24793a18d65ec8cc10f65c575f3d24d4641d1
moinultm <moinultm@gmail.com>
2019-08-19 09:41:34 +0600

    added migration all

--- app/Warehouse.php
+++ app/Warehouse.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Warehouse extends Model
 {
-    //
+    public function transactions () {
+        return $this->hasMany('App\Transaction');
+    }
 }
---
+++ database/migrations/2019_08_19_033142_create_purchases_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('reference_no');
+            $table->integer('client_id');
+            $table->integer('product_id');
+            $table->integer('warehouse_id')->default(1);
+            $table->double('quantity');
+            $table->double('sub_total');
+
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
