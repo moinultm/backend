aca8ed667d334f1b099c771da1acf6f79686addb
moinultm <moinultm@gmail.com>
2019-08-19 09:43:20 +0600

    added migration all

--- database/migrations/2019_08_19_033342_create_settings_table.php
+++ database/migrations/2019_08_19_033342_create_settings_table.php
@@ -15,6 +15,25 @@ class CreateSettingsTable extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('site_name');
+            $table->string('slogan')->nullable();
+            $table->string('address');
+            $table->string('email', 100)->nullable();
+            $table->string('phone')->nullable();
+            $table->string('owner_name')->nullable();
+            $table->text('site_logo')->nullable();
+            $table->string('currency_code')->nullable();
+            $table->integer('alert_quantity')->nullable();
+            $table->boolean('invoice_tax')->default(0);
+            $table->double('invoice_tax_rate')->default(0);
+            $table->integer('invoice_tax_type')->default(2);
+            $table->string('theme')->default("admin-lte-3");
+            $table->string('vat_no')->nullable();
+            $table->boolean('enable_purchaser')->default(1);
+            $table->boolean('enable_customer')->default(1);
+            $table->string('pos_invoice_footer_text')->nullable();
+            $table->string('dashboard')->default('chart-box');
+            $table->boolean('product_tax')->default(0);
             $table->timestamps();
         });
     }
