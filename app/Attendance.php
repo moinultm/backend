effc7a6d54a53285d10c7519ad6b1fea4021dc9d
moinultm <moinultm@gmail.com>
2019-11-06 21:57:53 +0600

    added username

--- app/Attendance.php
+++ app/Attendance.php
@@ -4,7 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Attendance extends Model
 {
-    //
+
+    protected $appends = array( 'user_name'  );
+
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class,'employee_id');
+    }
+
+    public function getUserNameAttribute()
+    {
+        $ret= $this->user()->select('name')->where('id', $this->employee_id)->pluck('name');
+        return $ret;
+    }
+
+
 }
